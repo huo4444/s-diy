f041914bac73ee577eb359a21dff5c53bd53e110
OF2597 <huochuang@qianmi.com>
2017-08-11 18:05:13 +0800

    init

---
+++ s-diy-crtl-ui/src/modules/editPage/widgets/widgetModule/src/widget/components/editableCom1/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/8/11 0011.
+ */
+import HOCTest2 from '../../../../../HOCTest2.edit'
---
+++ s-diy-crtl-ui/src/modules/editPage/widgets/widgetModule/src/widget/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/8/11 0011.
+ */
+import HOCTest from '../../../HOCTest.edit'
